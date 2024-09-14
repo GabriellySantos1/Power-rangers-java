148fc22c8995ccb60d4013024b08d1e9a027cc76
Gabrielly Santos <113446702+GabriellySantos1@users.noreply.github.com>
2024-09-13 21:11:06 -0300

    Add files via upload

---
+++ Power/src/RangerAmarelo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class RangerAmarelo extends Ranger {
+    
+    public void usarPoderAcelerador(){}
+
+
+}
---
+++ Power/src/Zord.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class Zord {
+ 
+    String nome;
+    String poder;
+    
+    public void atacar(){}
+    
+    
+}
